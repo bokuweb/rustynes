d1fd13980fccb745b888a79e4c835bb550cc83e5
bokuweb <bokuweb12@gmail.com>
2017-11-28 10:53:30 +0900

    Add key event handler

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 (async () => {
   // const main = Module.cwrap('run');
   const run = Module.cwrap('run', null, ['number', 'number']);
@@ -10,12 +11,40 @@
   const res = await fetch('./roms/giko008.nes');
   const arrayBuf = await res.arrayBuffer();
   const nes = new Uint8Array(arrayBuf);
-  const size = nes.byteLength;
+  // Add key code area to tail.
+  const size = nes.byteLength + 1;
   const ptr = Module._malloc(size);
   const buf = new Uint8Array(Module.HEAPU8.buffer, ptr, size);
   for (let i = 0; i < nes.length - 1; i = i + 1) {
     buf[i] = nes[i];
   }
+
+  const convertKeyCode = (keyCode) => {
+    switch (keyCode) {
+      case 88: return 0x01; // X  A 
+      case 90: return 0x02; // Z  B
+      case 65: return 0x04; // A  SELECT
+      case 83: return 0x08; // S  START
+      case 38: return 0x10; // ↑  ↑  
+      case 40: return 0x20; // ↓  ↓
+      case 37: return 0x40; // ←  ←
+      case 39: return 0x80; // →  →
+    }
+  };
+
+  const setupKeyHandler = () => {
+    if (typeof window !== 'undefined') {
+      document.addEventListener('keydown', (event) => {
+        console.log('keydown');
+        buf[size - 1] |= convertKeyCode(event.keyCode);
+      });
+
+      document.addEventListener('keyup', (event) => {
+        buf[size - 1] &= ~convertKeyCode(event.keyCode);
+      });
+    }
+  };
+  setupKeyHandler();
   run(size, buf.byteOffset);
   // main();
 })().catch(e => {
@@ -24,7 +53,6 @@
     return;
   }
 });
-
 
 
 
--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,13 @@ fn main() {}
 
 #[no_mangle]
 pub fn run(len: usize, ptr: *mut u8) {
-    let buf: &mut [u8] = unsafe { std::slice::from_raw_parts_mut(ptr, len) };
+    let buf: &mut [u8] = unsafe { std::slice::from_raw_parts_mut(ptr, len + 1) };
+    println!("{} {:?}", len, buf);
     let mut ctx = Context::new(buf);
     nes::reset(&mut ctx);
-    let main_loop = || { nes::run(&mut ctx); };
+    let main_loop = || {
+        println!("key code = {}", buf[len - 1]);
+        nes::run(&mut ctx);
+    };
     externs::set_main_loop_callback(main_loop);
 }
