539e90cf592b69737f591f9519ff731631bd0f1c
bokuweb <bokuweb12@gmail.com>
2017-12-24 16:38:08 +0900

    Add sprite horizontal / vertical reverse

--- src/nes/renderer/mod.rs
+++ src/nes/renderer/mod.rs
@@ -22,28 +22,40 @@ pub fn render(background: &BackgroundField, sprites: &SpritesWithCtx) {
 fn render_background(buf: &mut Vec<u8>, background: &BackgroundField) {
     for (i, bg) in background.into_iter().enumerate() {
         let x = (i % 33) * 8;
-        let y  = (i / 33) * 8;
+        let y = (i / 33) * 8;
         render_tile(buf, bg, x, y);
     }
 }
 
 fn render_sprites(buf: &mut Vec<u8>, sprites: &SpritesWithCtx) {
     for sprite in sprites {
-        render_sprite(buf, &sprite.sprite, &sprite.position, &sprite.palette);
+        render_sprite(buf,
+                      &sprite.sprite,
+                      &sprite.position,
+                      &sprite.palette,
+                      sprite.attr);
     }
 }
 
 fn render_sprite(data: &mut Vec<u8>,
                  sprite: &Sprite,
                  position: &SpritePosition,
-                 palette: &PaletteList) {
+                 palette: &PaletteList,
+                 attr: u8) {
+    let is_vertical_reverse = (attr & 0x80) == 0x80;
+    let is_horizontal_reverse = (attr & 0x40) == 0x40;
+    let is_low_priority = (attr & 0x20) == 0x20;
+    let palette_id = attr & 0x03;
     for i in 0..8 {
         for j in 0..8 {
+            let x = position.0 as usize + if is_horizontal_reverse { 7 - j } else { j };
+            let y = position.1 as usize + if is_vertical_reverse { 7 - i } else { i };
+            // if is_low_priority && this.shouldPixelHide(x, y)) {
+            //   continue;
+            // }
             if sprite[i][j] != 0 {
                 let color_id = palette[sprite[i][j] as usize];
                 let color = COLORS[color_id as usize];
-                let x = position.0 as usize + j;
-                let y = position.1 as usize + i;
                 let index = (x + (y * 0x100)) * 4;
                 data[index] = color.0;
                 data[index + 1] = color.1;
