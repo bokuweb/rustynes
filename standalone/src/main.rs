e1b1f216d305a90154fc0c2a386f15b9cb057567
tyfkda <tyfkda@gmail.com>
2019-06-24 09:52:11 +0900

    Load .nes file and initialize nes module

--- standalone/src/main.rs
+++ standalone/src/main.rs
@@ -1,7 +1,17 @@
 extern crate rustynes;
 
+use std::fs;
 use rustynes::nes;
+use rustynes::nes::Context;
 
 fn main() {
-    println!("Hello!");
+    match fs::read("roms/falling.nes") {
+        Result::Ok(mut rom) => {
+            let mut ctx = Context::new(&mut rom);
+            nes::reset(&mut ctx);
+        },
+        Result::Err(err) => {
+            panic!(err);
+        }
+    }
 }
